22584aacd7de4ef8bd8dfcd65481076cd132f013
Oaie Andrea-Beatriz <oaie.andrea.beatriz@gmail.com>
2023-01-26 14:48:14 +0200

    Update index.tsx

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -129,7 +129,7 @@ const Benefits = ({ setSelectedPage }:Props) => {
             {/* BUTTON */}
               <div className="relative mt-16">
                 <div className="before:absolute before:-bottom-20 before:right-20 before:z-[-1] before:content-eyeopen">
-                  <ActionButton setSelectedPage={SelectedPage}>Join Now</ActionButton>
+                  <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
                 </div>
               </div>
           </div>
