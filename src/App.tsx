0dc2bcbc1bdea2b21284832f74da2dd3ea95638f
Oaie Andrea-Beatriz <oaie.andrea.beatriz@gmail.com>
2023-01-30 13:07:39 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,14 +19,12 @@ function App() {
     const handleScroll = () => {
       if (window.scrollY === 0) {
         setIsTopOfPage(true);
-        setSelectedPage(SelectedPage.Home)
+        setSelectedPage(SelectedPage.Home);
       }
-      if (window.scrollY !== 0) {
-        setIsTopOfPage(false)
-      }
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll); 
-    }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return <div className='app bg-gray-20'>
